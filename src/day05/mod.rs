dd3a626c2611d5612e749c34b618ef1b3723cdb0
Stef Böhm <regendo@users.noreply.github.com>
2020-12-07 08:00:57 +0100

    Day 5 init

---
+++ src/day05/mod.rs
@@ -0,0 +1,7 @@
+pub fn solve_1() {
+	todo!()
+}
+
+pub fn solve_2() {
+	todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use advent_of_code_2020::*;
 
 fn main() {
-	day04::solve_1();
-	// day04::solve_2();
+	day05::solve_1();
+	day05::solve_2();
 }
